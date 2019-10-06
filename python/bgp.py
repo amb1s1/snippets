eab766b546999dafe16533cc070feef0d62bae87
David Gomez <amb1s1@gmail.com>
2019-10-06 19:34:37 +0000

    move python script to a new pyhton dir

---
+++ python/bgp.py
@@ -0,0 +1,37 @@
+###[ Ethernet ]###
+  dst= 3c:37:86:47:cd:85
+  src= f0:18:98:99:f8:08
+  type= 0x800
+###[ IP ]###
+     version= 4
+     ihl= None
+     tos= 0x0
+     len= None
+     id= 1
+     flags=
+     frag= 0
+     ttl= 1
+     proto= tcp
+     chksum= None
+     src= 1.1.1.1
+     dst= 1.1.1.2
+     \options\
+###[ TCP ]###
+        sport= 46612
+        dport= bgp
+        seq= 276039
+        ack= 309947
+        dataofs= None
+        reserved= 0
+        flags= PA
+        window= 16384
+        chksum= None
+        urgptr= 0
+        options= []
+###[ OPEN ]###
+           version= 4
+           my_as= 65010
+           hold_time= 180
+           bgp_id= 1.1.1.1
+           opt_param_len= 16
+           \opt_params\
---
+++ python/ospf.py
@@ -0,0 +1,27 @@
+###[ Ethernet ]###
+  dst= 3c:37:86:47:cd:85
+  src= f0:18:98:99:f8:08
+  type= 0x800
+###[ IP ]###
+     version= 4
+     ihl= None
+     tos= 0x0
+     len= None
+     id= 1
+     flags=
+     frag= 0
+     ttl= 1
+     proto= 89
+     chksum= None
+     src= 3.3.3.1
+     dst= 3.3.3.2
+     \options\
+###[ OSPF Hello ]###
+        mask= 255.255.255.0
+        hellointerval= 10
+        options=
+        prio= 1
+        deadinterval= 40
+        router= 0.0.0.0
+        backup= 0.0.0.0
+        neighbors= [3.3.3.2]
